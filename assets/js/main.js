36c561c0f2368738789fe683bc61128c2b31ed0f
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-18 12:15:25 +0200

    update the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,86 @@
-console.log("Hello world");
+// Setup
+
+let allMarkup = "";
+
+// seleziono gli elementi dalla DOM
+const rowEL = document.querySelector(".row");
+// console.log(imgEl);
+
+// chiamata AJAX
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+.then(response => {
+
+    // salvo la risposta in una costante
+    const photos = response.data
+    // console.log(photos);
+
+    // ciclo all'interno dell'array per ottenere accesso ai singoli oggetti        
+    photos.forEach(obj => {
+
+        // destrotturo l'oggetto per ricavare i dati che mi servono            
+        const { title, url } = obj
+        // console.log(title, url);
+
+        // invoco la variabile per creare il markup con i dati 
+        const markup = getMarkup(title, url);
+
+        // aggiungo il markup alla DOM
+        allMarkup += markup
+    })
+
+    rowEL.innerHTML = allMarkup;
+
+}) .catch(error => console.error(error))
+
+
+
+function getMarkup(title, url) {
+    return `
+    <!-- Card -->
+    <div class="card col-4">
+
+        <!-- pin img -->
+        <div class="pin">
+            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_d_11_3)">
+                    <circle cx="17" cy="16" r="16" fill="url(#paint0_linear_11_3)" />
+                    <circle cx="17" cy="16" r="15" stroke="url(#paint1_linear_11_3)" stroke-width="2" />
+                </g>
+                <defs>
+                    <filter id="filter0_d_11_3" x="0.6" y="0" width="34.8" height="35.4"
+                        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix"
+                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dx="1" dy="2" />
+                        <feGaussianBlur stdDeviation="0.7" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_11_3" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_11_3"
+                            result="shape" />
+                    </filter>
+                    <linearGradient id="paint0_linear_11_3" x1="17" y1="0" x2="17" y2="32"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#5D0B03" />
+                        <stop offset="1" stop-color="#F66659" />
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_11_3" x1="10.1429" y1="7.76563e-07" x2="30.2571"
+                        y2="24.6857" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FFF3EB" />
+                        <stop offset="1" stop-color="#512312" />
+                    </linearGradient>
+                </defs>
+            </svg>
+        </div>
+        <!-- card img -->
+        <div class="card-img">
+            <img src="${url}" alt="">
+        </div>
+        <!-- card body -->
+        <div class="card-body">
+            <p>${title}</p>
+        </div>
+    </div>
+    `;
+};
